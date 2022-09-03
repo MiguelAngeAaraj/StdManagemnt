13c771f169402fa268bacf3faf20bcc77f4021df
Miguel Ange <miguelangeaaraj77@gmail.com>
2022-09-03 10:28:40 +0300

    newFiles

--- src/Pages/EditStudent.jsx
+++ src/Pages/EditStudent.jsx
@@ -41,7 +41,7 @@ return (
       lastName:Lastname,
       age:Age}
       try{ 
-      await axios.put(`/people`,Form,{
+      await axios.put(`https://peopleleb.herokuapp.com/people`,Form,{
         headers:{
           'Content-Type':'application/json'
         }
